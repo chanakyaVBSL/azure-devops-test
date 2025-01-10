ff9b74805155f8271185a7ff820892983e7c6383
Chanakya VBSL <vbsl.chanakya6@gmail.com>
2025-01-10 08:25:14 +0530

    Add files via upload
    Java File

---
+++ num_sum.java
@@ -0,0 +1,7 @@
+public class num_sum {
+	public static void main(String[] args) {
+		int a = 50, b = 75;
+		System.out.println("Java Program");
+		System.out.println("Sum of " + a + " and " + b + " is " + (a + b));
+	}
+}
